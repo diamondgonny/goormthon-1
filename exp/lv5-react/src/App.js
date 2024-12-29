cf3b5c03ca3ccd834e5007e86788c15fbf2faa6c
Kim Keon-Young <diamondgonny@gmail.com>
2024-12-29 17:35:54 +0900

    feat: Add codes to submit Lv5 mission 2(practice)

--- exp/lv5-react/src/App.js
+++ exp/lv5-react/src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css";
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [isOn, setIsOn] = useState(true);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Counter</h1>
+        <h2 data-testid="counter">{counter}</h2>
+        <div className="button-row">
+          <button
+            data-testid="minus-button"
+            onClick={() => setCounter(counter - 1)}
+            disabled={!isOn}
+          >-</button>
+          <button
+            data-testid="plus-button"
+            onClick={() => setCounter(counter + 1)}
+            disabled={!isOn}
+          >+</button>
+        </div>
+        <div className="switch-container">
+          <button
+            data-testid="on/off-button"
+            onClick={() => setIsOn(!isOn)}
+            style={{ backgroundColor: isOn ? 'blue' : 'red' }}
+          >
+            on/off
+          </button>
+        </div>
       </header>
     </div>
   );
--- exp/lv5-react/src/App.test.js
+++ exp/lv5-react/src/App.test.js
@@ -1,8 +1,40 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test('the counter decreases by 1 when the - button is clicked', () => {
+  render(<App />);
+  const minusButton = screen.getByTestId('minus-button');
+  fireEvent.click(minusButton);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(-1);
+});
+
+test('the counter increases by 1 when the + button is clicked', () => {
+  render(<App />);
+  const plusButton = screen.getByTestId('plus-button');
+  fireEvent.click(plusButton);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test('the on/off button has blue color', () => {
+  render(<App />);
+  const onOffButton = screen.getByTestId('on/off-button');
+  expect(onOffButton).toHaveStyle({ backgroundColor: 'blue' });
+});
+
+test('prevent the -, + button from being clicked when the on/off button is clicked', () => {
+  render(<App />);
+  const onOffButton = screen.getByTestId('on/off-button');
+  fireEvent.click(onOffButton);
+  const minusButton = screen.getByTestId('minus-button');
+  const plusButton = screen.getByTestId('plus-button');
+  expect(minusButton).toBeDisabled();
+  expect(plusButton).toBeDisabled();
 });
